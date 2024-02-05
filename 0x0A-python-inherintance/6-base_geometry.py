e471d19cb719cc72cb7c76804db6fed3fb832990
blessi30ng <blessingchidewu3@gmail.com>
2024-02-04 23:51:34 -0800

    lame

---
+++ 0x0A-python-inherintance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhon3
+"""Defines a base geometry class BaseGeometry"""
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """Area not  implemented"""
+        raise Exception("area() is not implemented")
