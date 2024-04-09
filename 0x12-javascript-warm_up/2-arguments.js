ec9cf2193eeb472200082d41ee0f81eb165a470a
blessi30ng <blessingchidewu3@gmail.com>
2024-04-09 05:59:50 -0700

    lol

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const lol = process.argv.length;
-if (lol === 2) {
+const lol = process.argv.length - 2;
+if (lol === 0) {
 	console.log('No arguments');
-} else if (lol === 3) {
+} else if (lol === 1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
