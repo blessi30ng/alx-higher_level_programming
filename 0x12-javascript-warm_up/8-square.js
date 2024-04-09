f56c1cd391a7e80ed0e45c8c8ba0b2944feb2cd0
blessi30ng <blessingchidewu3@gmail.com>
2024-04-09 07:40:37 -0700

    lol

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const square = Number(process.argv[2]);
+if (isNaN(square)) {
+	console.log('Missing size');
+} else {
+	for (let i = 0; i < square; i++) {
+		let line = '';
+		for (let j = 0; j < square; j++) {
+			line += 'X';
+		}
+		console.log(line);
+	}
+}
