2bae0cfe6033e1d3dea1f0323d18506da5c1a364
blessi30ng <blessingchidewu3@gmail.com>
2024-04-15 06:58:57 -0700

    lol

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-""" 
+"""cities by states """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    if len(argv) != 5:
+        sys.exit('Use: 1-filter_states.py <mysql username> <mysql password>'
+                ' <database name> <state name searched>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s "
+            "ORDER BY id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
