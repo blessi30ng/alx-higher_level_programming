cde9887ea99403c9b802ada38aba69faed19201e
blessi30ng <blessingchidewu3@gmail.com>
2024-04-18 04:41:23 -0700

    lol

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""script that changes the name of a State object from the database hbtn_0e_6_usa"""
+"""script that changes the name of a State object from the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
