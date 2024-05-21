13d85b9293701cc23fc29fa219ca086022864fa3
blessi30ng <blessingchidewu3@gmail.com>
2024-05-21 04:53:49 -0700

    lol

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writefile(file, string, 'utf-8', function(err) {
-	if (err) console.log(err);
+fs.writefile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
