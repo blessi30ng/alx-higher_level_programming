74ea4e90a580d1fbab883b8fda09cb6c047fd592
blessi30ng <138776307+blessi30ng@users.noreply.github.com>
2024-06-18 12:27:48 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-	const rever = [];
-	for (let i = 0; i < list.length; i++) {
-		rever[list.length - i - 1] = list[i];
-	}
-	return rever;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-let lol = 0;
-exports.logMe = function (item) {
-	console.log(lol + ': ' + item);
-	lol++;
-};
