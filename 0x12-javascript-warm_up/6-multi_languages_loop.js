85c0965541b24a9fa2272f87b45fbfa2e13004aa
blessi30ng <blessingchidewu3@gmail.com>
2024-06-18 06:34:22 -0700

    lol

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// script that prints 3 lines
+
+const array = ['C is fun', 'Python is cool',  'Javascript is amazing'];
+
+let i = 0;
+
+while (i < array.length) {
+  console.log(array[i]);
+  i++;
+}
