8a1633382bcb4d577d2683dfc7e805c067c60240
blessi30ng <blessingchidewu3@gmail.com>
2024-06-19 06:12:20 -0700

    lol

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  costructor (size) {
+    super(size, size);
+  }
+}
+
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!usr/bin/node
+
+let nag = 0;
+
+exports.logMe = function (item) {
+  console.log(nag + ': ' + item);
+  nag++;
+};
