c99ed7fbedd747bd9efca80706e0ca0eef94d969
blessi30ng <blessingchidewu3@gmail.com>
2024-07-05 06:44:02 -0700

    lol

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays the body of the response
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+
+    re = requests.get(url)
+    if re.status_code >= 400:
+        print("Error code: {}".format(re.status_code))
+    else:
+        print(re.text)
